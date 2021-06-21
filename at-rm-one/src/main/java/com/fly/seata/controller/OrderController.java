1310863bfe96ba7dfc52e9b7f7a543a416a50d49
pangpeijie <pangpeijie@dxy.cn>
2021-06-21 10:34:57 +0800

    [MOD]seata调整接入模式

--- at-rm-one/src/main/java/com/fly/seata/controller/OrderController.java
+++ at-rm-one/src/main/java/com/fly/seata/controller/OrderController.java
@@ -2,7 +2,6 @@ package com.fly.seata.controller;
 
 import com.fly.seata.domain.Order;
 import com.fly.seata.service.OrderService;
-import io.seata.spring.annotation.GlobalTransactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +19,6 @@ public class OrderController {
   @Autowired
   private OrderService orderService;
 
-  @GlobalTransactional(lockRetryInternal = 10,lockRetryTimes = 30)
   @PostMapping(value = "/order/create",consumes = MediaType.APPLICATION_JSON_VALUE)
   public String createOrder(@RequestBody Order order){
     orderService.createOrder(order);
